e92aaed744da731055711cf3a03e27c13c8d92cc
ahmed <ag3204123@gmail.com>
2025-08-17 06:01:34 -0700

    fix

--- utils/token/createToken.js
+++ utils/token/createToken.js
@@ -9,7 +9,7 @@ export const createToken = (user, statusCode, res) => {
   res.cookie("jwt", token, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     secure: process.env.NODE_ENV !== "development",
   });
 
